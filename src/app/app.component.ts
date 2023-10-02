a049493a70680d238bae86c33eb1df8626411d82
ArunArunAk <arunarun2gs@gmail.com>
2023-10-02 17:29:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
 import { Component } from '@angular/core';
+import { Firestore, collection, addDoc,collectionData, doc ,deleteDoc} from '@angular/fire/firestore';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'fire';
+  userdata!:Observable<any>;
+  array=[];
+
+  constructor(public toastr: ToastrService,private firestore: Firestore) {//
+    this.getData();
+    console.log("userdata",this.userdata);
+
+  }
+  onSubmit(form: any) {
+    console.log('Form submitted:', form.value);
+  
+    const collectionInstance = collection(this.firestore, 'users');
+    addDoc(collectionInstance, form.value)
+      .then(() => {
+        form.reset();
+        console.log('data saved successfully');
+        this.toastr.success('Data added successfully!', 'Success');
+        
+      })
+      .catch((err) => {
+        console.log(err);
+        this.toastr.error('Error adding data!', 'Error');
+      });
+  }
+  getData(){
+    const collectionInstance = collection(this.firestore, 'users');
+     collectionData(collectionInstance,{ idField:'id'}).subscribe(val=>{
+      console.log(val);
+     })
+     this.userdata=collectionData(collectionInstance,{ idField:'id'});
+  }
+
+  dletedata(id: string) {
+    const docInstance = doc(this.firestore, 'users', id);
+    deleteDoc(docInstance).then(() => {
+      console.log('Delete successful');
+      this.toastr.info('Data deleted successfully!', 'sucesss');
+    }).catch((err) => {
+      console.log(err);
+      this.toastr.error('Error deleting data!', 'Error');
+    });
+  }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'ecommerce-ba808',
+    appId: '1:980899440077:web:b4a93d248f46df81dc9084',
+    storageBucket: 'ecommerce-ba808.appspot.com',
+    apiKey: 'AIzaSyCO2Ly8Z3mMG6p_8VElrxPGEZlMeO50jpQ',
+    authDomain: 'ecommerce-ba808.firebaseapp.com',
+    messagingSenderId: '980899440077',
+  },
+};
